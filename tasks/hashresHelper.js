b336e96a924b2ef252d64b92608030b70f8cb1c9
Bryce Kahle <bryce.kahle@mlssoccer.com>
2014-07-16 14:26:18 -0400

    Force to match full quoted strings

--- tasks/hashresHelper.js
+++ tasks/hashresHelper.js
@@ -93,16 +93,16 @@ exports.hashAndSub = function(grunt, options) {
         var destContents = fs.readFileSync(f, encoding);
         files.forEach(function(value) {
           grunt.log.debug('Substituting ' + value[0] + ' by ' + value[1]);
-          destContents = destContents.replace(new RegExp(utils.preg_quote(value[0])+"(\\?[0-9a-z]+)?", "g"), function () {
+          destContents = destContents.replace(new RegExp(utils.preg_quote('[\'"]' + value[0])+'(\\?[0-9a-z]+)?[\'"]', 'g'), function () {
             grunt.log.write(value[0] + ' ').ok(value[1]);
             return value[1];
           });
 
-          // grunt.log.debug('Substituting ' + nameToNameSearch[value[0]] + ' by ' + value[1]);
-          // destContents = destContents.replace(
-          //       new RegExp(nameToNameSearch[value[0]], "g"),
-          //       value[1]
-          //   );
+          grunt.log.debug('Substituting ' + nameToNameSearch[value[0]] + ' by ' + value[1]);
+          destContents = destContents.replace(
+                new RegExp('[\'"]' + nameToNameSearch[value[0]] + '[\'"]', "g"),
+                value[1]
+            );
         });
         grunt.log.debug('Saving the updated contents of the destination file');
         fs.writeFileSync(f, destContents, encoding);
